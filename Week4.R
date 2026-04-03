23d13ff04d94f4c89bf572c2b586b5f2134c0b4c
HyeRyeong125 <aksux0199@gmail.com>
2026-04-03 11:04:53 +0900

    Week5.R

--- Week4.R
+++ Week4.R
@@ -229,3 +229,122 @@ a3 = array(c(1:12), dim=c(2,2,3))
 a4 = aperm(a3, dim=c(2,1,3))
 a4
 
+
+#특정 키만 조회
+l1$addr
+
+#list에 요소 추가/삭제
+l1$birth = '2026'
+l1$birth
+
+l1$name
+l1$name = c('고길동', '마이콜')
+l1$name
+l1$name[length(l1$name)+1, '둘리']
+l1$name
+
+#append() 사용
+l1$name = append(l1$name, '홍길동', after=1)
+l1$name
+
+#list 삭제
+#1.특정값을 삭제
+l1$name[length(l1$name)-1]=NA
+l1$name
+
+#2. 리스트 키 삭제
+l1$birth=NULL
+
+#2. dataframe
+#2-1. 각 컬럼(라벨)별로 먼저 생성 후 data.frame으로 모든 컬럼을 합친다
+no = c(1,2,3,4)
+name = c('Apple', 'Banana', 'Peach', 'Grape')
+price = c(500,200,100,300)
+qty = c(5,2,4,7)
+
+sales = data.frame(NO=no, NAME=name, PRICE=price, QTY=qty)
+sales
+class(sales)
+str(sales)
+
+#2-2. 행렬로 생성
+sales2 = matrix(c(1, 'Apple', 500, 5,
+                  2, 'Peach', 200, 2,
+                  3, 'Banana', 100, 4,
+                  4, 'Grape', 50, 7), 4, by=T)
+sales2
+d1 = data.frame(sales2)
+d1
+
+names(d1) = c('NO', 'NAME', 'PRICE', 'QTY')
+d1
+
+#데이터 조회
+sales
+sales$NAME
+sales[1,3]
+sales[, 2]
+sales[3,]
+sales[c(1,3)]
+sales[c(1,3),]
+sales[,c(2,4)]
+sales[,c(1:3)]
+
+#원하는 조건만 검색 : subset()
+sales
+subset(sales, qty <= 5)
+subset(sales, price == 200)
+subset(sales, name == 'Apple')
+
+#데이터 추가/합치기 : rbind(), cbind(), merge()
+no = c(1,2,3)
+name = c('apple', 'banana', 'peach')
+price = c(100,200,300)
+
+df1 = data.frame(NO=no, NAME=name, PRICE=price)
+df1
+
+no = c(10,20,30)
+name = c('train', 'car', 'ship')
+price = c(1000,2000,3000)
+
+df2 = data.frame(NO=no, NAME=name, PRICE=price)
+df2
+
+#컬럼명 동일시 r, c 모두 bind해도 상관없음
+df3 = cbind(df1, df2)
+df3
+
+df4 = rbind(df1, df2)
+df4
+
+#컬럼명 다를 시, cbind는 가능하지만 rbind는 불가함
+df5 = data.frame(name=c('apple', 'banana', 'cherry'), price = c(300,200,100))
+df6 = data.frame(name=c('apple', 'cherry', 'berry'), qty = c(10,20,30))
+cbind(df5,df6)
+rbind(df5, df6)
+
+#merge시, df5와 df6에서 공통적으로 있는 내용이 합쳐지고, 다른 내용은 삭제됨
+merge(df5, df6)
+merge(df5, df6, all = T) #all = T 실행시 전체 내용이 다 합쳐짐.없는 내용은 NA처리
+
+#번호가 4, 5이고, 이름이 'mango', 'berry'와 가격이 각 400, 500인 데이터 생성 후 df1행 추가
+no = c(4,5)
+name = c('mango', 'berry')
+price = c(400,500)
+df7 = data.frame(NO=no, NAME=name, PRICE=price)
+df7 = rbind(df7, df1)
+
+#qty(수량)이 (10,20,30,40,50)인 데이터를 열추가 하시오
+df7 = cbind(df7, qty = c(10,20,30,40,50))
+
+class(df7)
+str(df7)
+
+#데이터 수정 : 변수명 바꾸기
+install.packages("dplyr")
+library(dplyr)
+
+install.packages("installr")
+library(installr)
+check.for.updates.R()
